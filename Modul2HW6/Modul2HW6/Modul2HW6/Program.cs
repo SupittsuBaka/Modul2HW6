6478735e0d90e253aad82444f5576a11de1b3743
Admin-PC\Admin <sashashmidt02@gmail.com>
2021-07-22 21:06:49 +0300

    Result

--- Modul2HW6/Modul2HW6/Modul2HW6/Program.cs
+++ Modul2HW6/Modul2HW6/Modul2HW6/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-
-namespace Modul2HW6
+﻿namespace Modul2HW6
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var starter = new AppStarter();
+            starter.Run();
         }
     }
 }
